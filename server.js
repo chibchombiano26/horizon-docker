c9908dde3d7e3ea67f162efdcd6b8f70ac399352
Jose douglas ramirez e <jose.douglas.ramirez@outllok.com>
2016-08-06 11:18:09 -0500

    Server

--- server.js
+++ server.js
@@ -17,16 +17,42 @@ const httpServer = app.listen(8081, function(err) {
     }
 });
 
+let autoCreateCollection = true;
+let auto_create_index = true;
+let permission = false;
+let allow_anonymous = true;
+let allow_unauthenticated = true;
+
+if(process.env.CREATE_COLLECTION){
+    autoCreateCollection = process.env.CREATE_COLLECTION;
+}
+
+if(process.env.CREATE_INDEX){
+    auto_create_index = process.env.CREATE_INDEX;
+}
+
+if(process.env.PERMISSION){
+    permission = process.env.PERMISSION;
+}
+
+if(process.env.ALLOW_ANONYMOUS){
+    allow_anonymous = process.env.ALLOW_ANONYMOUS;
+}
+
+if(process.env.ALLOW_UNAUTHENTICATED){
+    allow_unauthenticated = process.env.ALLOW_UNAUTHENTICATED;
+}
+
 const horizonServer = horizon(httpServer, {
-    auto_create_collection: process.env.CREATE_COLLECTION | true,
-    auto_create_index: process.env.CREATE_INDEX | true,
+    auto_create_collection: autoCreateCollection,
+    auto_create_index: auto_create_index,
     project_name: process.env.PROJECT_NAME || 'HorizonWithDocker',
-    permissions: process.env.PERMISSION | false,
+    permissions: permission,
     rdb_host: process.env.RDB_HOST || 'localhost',
     rdb_port: process.env.RDB_PORT || 28015,
     auth: {
-        allow_anonymous: process.env.ALLOW_ANONYMOUS || true,
-        allow_unauthenticated: process.env.ALLOW_UNAUTHENTICATED || true,
+        allow_anonymous: allow_anonymous,
+        allow_unauthenticated: allow_unauthenticated,
         token_secret: process.env.TOKEN_SECRET || 'HorizonWithDockerIsSecret'
     }
 });
